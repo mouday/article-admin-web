40bc4ec0050e8e90f0389526aaef180afe11364d
mouday <pengshiyuyx@gmail.com>
2024-06-16 12:35:21 +0000

    Deploying to preview from @ mouday/article-admin-web@fb8ec2aa3ac5d329fec482c9ecfea4baac20a67d 🚀

---
+++ assets/Log-2fe75533.js
@@ -0,0 +1,1 @@
+import{bj as i,a0 as l,bk as u,r as s,j as t,a1 as g}from"./index-495a2bcd.js";function p(){i();const[e,m]=l();console.log(e);const{taskLogId:o}=u(),[r,c]=s.useState(""),n=async()=>{const a=await g.getTaskLogDetail({taskLogId:o});a.ok&&c(a.data)};return s.useEffect(()=>{n()},[]),t.jsx("div",{className:"log-wrap",children:t.jsx("pre",{children:r})})}export{p as default};
